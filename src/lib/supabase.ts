dce29de0ee901f02f2d1d82c65d695f909005a13
testtestingg <dridixhydra@gmail.com>
2026-02-26 18:49:15 +0100

    Updated supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,8 @@
-// src/lib/supabase.ts
+// src/supabase.ts
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_CALENDAR_SUPABASE_URL || 'https://yygjaphftialpvwocnzi.supabase.co';
-const supabaseAnonKey = import.meta.env.VITE_CALENDAR_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl5Z2phcGhmdGlhbHB2d29jbnppIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY5MDM1NzMsImV4cCI6MjA3MjQ3OTU3M30.cel7Z-iC50ESr9QpRLbxMXhbmzBYaCdKv_3e21K4Q_g';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+// The "export" here is required so ArchiveBac can use it!
+export const supabase = createClient(supabaseUrl, supabaseKey);
