0a774acecb682cfcf40094dab36384ffd5e6f9d3
testtestingg <dridixhydra@gmail.com>
2026-02-26 19:04:01 +0100

    Deleted supabase.ts

--- src/supabase.ts
+++
@@ -1,8 +0,0 @@
-// src/supabase.ts
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-
-// The "export" here is required so ArchiveBac can use it!
-export const supabase = createClient(supabaseUrl, supabaseKey);
