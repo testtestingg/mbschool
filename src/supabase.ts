4d3b8392703acbfe65f90917d72e3b17420dcbf8
testtestingg <dridixhydra@gmail.com>
2026-02-26 18:06:46 +0100

    Updated supabase.ts

--- src/supabase.ts
+++ src/supabase.ts
@@ -4,5 +4,5 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-// This ensures we only create ONE client for the whole app
+// The "export" here is required so ArchiveBac can use it!
 export const supabase = createClient(supabaseUrl, supabaseKey);
